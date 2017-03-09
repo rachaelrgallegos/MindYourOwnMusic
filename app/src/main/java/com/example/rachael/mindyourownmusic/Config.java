ff3371e6af3b73ed4335089b79728ba4bb1f903b
Rachael <rachaelrgallegos@gmail.com>
2017-03-08 18:28:20 -0700

    Initial

---
+++ app/src/main/java/com/example/rachael/mindyourownmusic/Config.java
@@ -0,0 +1,14 @@
+package com.example.rachael.mindyourownmusic;
+
+/**
+ * Created by racha on 3/8/2017.
+ */
+
+public class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyBpI2RodNvtGk0mgsIkZjCOjCREzVk8mHA";
+
+}
